12d43c4681eeaa47751bf22aa98334e07e261a55
RyanJohnson1612 <ryanjohnson@rogers.com>
2021-12-06 22:48:39 +0000

    Add route and template for new urls

--- express_server.js
+++ express_server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// set view engine to use ejs
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+
+/****************************
+  Routes
+ ****************************/
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -18,10 +23,15 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
